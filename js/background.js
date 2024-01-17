dad672e00b2e4dc4e0601dae20ae4ca9da53994f
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2024-01-17 13:57:23 -0300

    fix data comphression

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
-// chrome.webRequest.onCompleted.addListener(
-//   logCompletedRequest,
-//   { urls: ["<all_urls>"] },
-//   ["responseHeaders", "extraHeaders"]
-// );
+chrome.webRequest.onCompleted.addListener(
+  logCompletedRequest,
+  { urls: ["<all_urls>"] },
+  ["responseHeaders", "extraHeaders"]
+);
 
 chrome.webRequest.onBeforeRequest.addListener(
   logCompletedRequest,
@@ -14,31 +14,38 @@ function logCompletedRequest(details) {
   chrome.storage.sync.get(['tabId'], function(items) {
     if (details.tabId == items.tabId && details.type === "xmlhttprequest") {
 
-      var requestInfo = {
-        requestId: details.requestId,
-        url: details.url,
-        method: details.method,
-        responseHeaders: details.responseHeaders,
-        statusCode: details.statusCode,
-        requestBody: details.requestBody,
-      };
+      var requestId = details.requestId;
 
-      saveRequestToChromeStorage(requestInfo);
+      // Verificar se já existe uma entrada para este requestId
+      if (!capturedRequests[requestId]) {
+        capturedRequests[requestId] = {
+          request: null,
+          response: null
+        };
+      }
+
+      // Verificar se é uma solicitação ou uma resposta
+      if (details.statusCode) {
+        // É uma resposta
+        capturedRequests[requestId].response = {
+          url: details.url,
+          method: details.method,
+          responseHeaders: details.responseHeaders,
+          statusCode: details.statusCode,
+        };
+      } else {
+        // É uma solicitação
+        capturedRequests[requestId].request = {
+          url: details.url,
+          method: details.method,
+          requestBody: details.requestBody,
+        };
+      }
+
+      console.log(capturedRequests[requestId]);
     }
   });
+  
 }
 
-function saveRequestToChromeStorage(requestInfo) {
-  console.log(requestInfo);
-  // Recuperar as informações já armazenadas no chrome.storage.sync (se houver)
-  chrome.storage.sync.get(['storedRequests'], function(data) {
-    var storedRequests = data.storedRequests || [];
-
-    // Adicionar a nova solicitação às informações armazenadas
-    storedRequests.push(requestInfo);
-
-    // Salvar as informações atualizadas no chrome.storage.sync
-    // chrome.storage.sync.set({'storedRequests': storedRequests}, function() {
-    // });
-  });
-}
+var capturedRequests = {};
