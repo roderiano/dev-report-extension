bd26040e8622c6ab90a9e1fdeaaacce73c04cc7e
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2024-01-17 17:24:34 -0300

    minor fixes

--- js/background.js
+++ js/background.js
@@ -17,7 +17,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 );
 
 async function logCompletedRequest(details) {
-  chrome.storage.sync.get(['tabId'], async function(items) {
+  chrome.storage.local.get(['tabId'], async function(items) {
     if (details.tabId == items.tabId && details.type === "xmlhttprequest") {
 
       var requestId = details.requestId;
@@ -29,7 +29,6 @@ async function logCompletedRequest(details) {
       }
 
       if (details.statusCode) {
-        // É uma resposta
         capturedRequests[requestId].response = {
           url: details.url,
           method: details.method,
@@ -37,26 +36,35 @@ async function logCompletedRequest(details) {
           statusCode: details.statusCode,
           responseBody: null
         };
-        const response = await fetch(details.url, {
+        
+        console.log(capturedRequests[requestId].request.requestHeaders);
+        await fetch(details.url, {
           method: details.method, 
           headers: formatHeaders(capturedRequests[requestId].request.requestHeaders),
           body: capturedRequests[requestId].request.requestBody
         })
         .then(response => response.text())
         .then(responseText => {
+          console.log(1);
           capturedRequests[requestId].response.responseBody = responseText;
+          console.log(2);
         })
-      } else if (!details.requestHeaders) {
+
+      } 
+      else if (!details.requestHeaders) {
         capturedRequests[requestId].request = {
           url: details.url,
           method: details.method,
           requestBody: details.requestBody,
         };
-      }
+      } 
       else if(details.requestHeaders) {
         capturedRequests[requestId].request.requestHeaders = details.requestHeaders;
       }
-      console.log(capturedRequests);
+      
+      chrome.storage.local.set({'tabRequests': capturedRequests}, function() {
+        console.log('Saved requests from tab ');
+      });
     }
   });
   
--- js/popup.js
+++ js/popup.js
@@ -16,22 +16,40 @@ document.addEventListener('DOMContentLoaded', function () {
 function startRecord() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const tabId = tabs[0].id;
-        chrome.storage.sync.set({'tabId': tabId}, function() {
+        chrome.storage.local.set({'tabId': tabId}, function() {
             console.log('Listening to tab ' + tabId);
+        });
+
+        chrome.storage.local.set({'tabRequests': []}, function() {
+            console.log('Cleaning data from tab ' + tabId);
         });
     });
     showCaptureScreen();
 }
 
 function stopRecord() {
-    chrome.storage.sync.set({'tabId': null}, function() {
+    chrome.storage.local.set({'tabId': null}, function() {
         console.log('Stop the listening...');
     });
+
+    chrome.storage.local.get(['tabRequests'], function(items) {
+        console.log(items);
+        const jsonData = JSON.stringify(items.tabRequests, null, 2); 
+        const blob = new Blob([jsonData], { type: 'application/json' });
+        const blobURL = URL.createObjectURL(blob);
+        const downloadLink = document.createElement('a');
+        downloadLink.href = blobURL;
+        downloadLink.download = 'tabRequests.json';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
+
     showStartScreen();
 }
 
 function routeScreen() {
-    chrome.storage.sync.get(['tabId'], function(items) {
+    chrome.storage.local.get(['tabId'], function(items) {
         if(!items.tabId) {
             showStartScreen();
         } else {
