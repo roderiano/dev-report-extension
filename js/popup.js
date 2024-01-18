3ab64c235716d6beceb2545854a5bf3346af7f3b
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2024-01-18 14:54:53 -0300

    code refactor

--- js/popup.js
+++ js/popup.js
@@ -1,106 +1,106 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    routeScreen();
+  routeScreen();
 
-    var startReportButton = document.getElementById("startReport");
-    startReportButton.addEventListener('click', function () {
-        startRecord();
-    });
+  var startReportButton = document.getElementById("startReport");
+  startReportButton.addEventListener('click', function () {
+    startRecord();
+  });
 
-    var stopReportButton = document.getElementById("stopReport");
-    stopReportButton.addEventListener('click', function () {
-        stopRecord();
-    });
+  var stopReportButton = document.getElementById("stopReport");
+  stopReportButton.addEventListener('click', function () {
+    stopRecord();
+  });
 
 });
 
 function startRecord() {
-    chrome.storage.local.set({'requests': []}, function() {
-        console.log("Requests deleted from memory");
+  chrome.storage.local.set({ 'requests': [] }, function () {
+    console.log("Requests deleted from memory");
+  });
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tabId = tabs[0].id;
+    chrome.storage.local.set({ 'tabId': tabId }, function () {
+      console.log('Listening to tab ' + tabId);
     });
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const tabId = tabs[0].id;
-        chrome.storage.local.set({'tabId': tabId}, function() {
-            console.log('Listening to tab ' + tabId);
-        });
-    });
-    
-    routeScreen();
+  });
+
+  routeScreen();
 }
 
 function loadJSZip(callback) {
-    const script = document.createElement('script');
-    script.src = chrome.runtime.getURL('./js/jszip.min.js');
-    script.onload = callback;
-    document.head.appendChild(script);
-  }
-  
-  function getFileContent(fileURL) {
-    return fetch(fileURL).then(response => response.text());
-  }
-  
-  function stopRecord() {
-    // Load JSZip dynamically
-    loadJSZip(function () {
-      chrome.storage.local.get('requests', async function (result) {
-        const jsonData = result.requests;
-  
-        // Fetch content of preview.html
-        const previewHtmlContent = await getFileContent(chrome.runtime.getURL('./html/preview.html'));
-  
-        // Create a zip file containing both preview.html and tabRequests.json
-        const zip = new JSZip();
-  
-        // Add preview.html to the zip
-        zip.file('preview.html', previewHtmlContent);
-  
-        // Add tabRequests.json to the zip
-        zip.file('tabRequests.js', "var data = " + jsonData);
-  
-        // Generate the zip file
-        zip.generateAsync({ type: 'blob' }).then(function (content) {
-          // Trigger download of the zip file
-          chrome.downloads.download({
-            url: URL.createObjectURL(content),
-            filename: 'extension_data.zip',
-            saveAs: false,
-            conflictAction: 'uniquify',
-            method: 'GET'
-          }, function (downloadId) {
-            if (chrome.runtime.lastError) {
-              console.error('Error downloading zip file:', chrome.runtime.lastError);
-            } else {
-              console.log('Zip file saved locally.');
-            }
-          });
+  const script = document.createElement('script');
+  script.src = chrome.runtime.getURL('./js/jszip.min.js');
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+function getFileContent(fileURL) {
+  return fetch(fileURL).then(response => response.text());
+}
+
+function stopRecord() {
+  // Load JSZip dynamically
+  loadJSZip(function () {
+    chrome.storage.local.get('requests', async function (result) {
+      const jsonData = result.requests;
+
+      // Fetch content of preview.html
+      const previewHtmlContent = await getFileContent(chrome.runtime.getURL('./html/preview.html'));
+
+      // Create a zip file containing both preview.html and tabRequests.json
+      const zip = new JSZip();
+
+      // Add preview.html to the zip
+      zip.file('preview.html', previewHtmlContent);
+
+      // Add tabRequests.json to the zip
+      zip.file('tabRequests.js', "var data = " + jsonData);
+
+      // Generate the zip file
+      zip.generateAsync({ type: 'blob' }).then(function (content) {
+        // Trigger download of the zip file
+        chrome.downloads.download({
+          url: URL.createObjectURL(content),
+          filename: 'extension_data.zip',
+          saveAs: false,
+          conflictAction: 'uniquify',
+          method: 'GET'
+        }, function (downloadId) {
+          if (chrome.runtime.lastError) {
+            console.error('Error downloading zip file:', chrome.runtime.lastError);
+          } else {
+            console.log('Zip file saved locally.');
+          }
         });
-  
-        chrome.storage.local.set({ 'tabId': null }, function () {
-          console.log('Stop the listening...');
-        });
-  
-        routeScreen();
       });
+
+      chrome.storage.local.set({ 'tabId': null }, function () {
+        console.log('Stop the listening...');
+      });
+
+      routeScreen();
     });
-  }
-  
+  });
+}
+
 
 function routeScreen() {
-    chrome.storage.local.get(['tabId'], function(items) {
-        if(!items.tabId) {
-            showStartScreen();
-        } else {
-            showCaptureScreen();
-        }
-    });
+  chrome.storage.local.get(['tabId'], function (items) {
+    if (!items.tabId) {
+      showStartScreen();
+    } else {
+      showCaptureScreen();
+    }
+  });
 }
 
 function showCaptureScreen() {
-    document.getElementById("start-screen").hidden = true;
-    document.getElementById("capture-screen").hidden = false;
+  document.getElementById("start-screen").hidden = true;
+  document.getElementById("capture-screen").hidden = false;
 }
 
 function showStartScreen() {
-    document.getElementById("start-screen").hidden = false;
-    document.getElementById("capture-screen").hidden = true;
+  document.getElementById("start-screen").hidden = false;
+  document.getElementById("capture-screen").hidden = true;
 }
